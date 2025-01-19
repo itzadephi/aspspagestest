1154d9db130d560b79d20e3ffc4c9e01f00e3e4b
Ade_V <90307161+itzadephi@users.noreply.github.com>
2025-01-19 13:26:48 +0800

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-let select_id = document.getElementById('select-id');
-let lets_go = document.getElementById('lets-go');
-
-select_id.addEventListener('input', function(ev){
-    lets_go.disabled = (select_id.value === 'Choose smth')
-});
